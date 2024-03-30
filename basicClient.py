1e4394d18713cc6bc2615e93f67c8e41e26c6339
cemacdonald <cemacdonald2@miners.utep.edu>
2024-03-29 19:08:02 -0600

    added my framer/deframer

--- basicClient.py
+++ basicClient.py
@@ -1,43 +1,42 @@
 #! /usr/bin/env python3
-
-import sys,time,re,socket
-
+import sys
+import socket
+import os
 
 def outBandFrame(files):
-    #error handling
+    framed_data = b""
     for file in files:
         if not os.path.exists(file):
             print(f"File {file} does not exist", file=os.fdopen(2, 'w'))
-            return
-        #opening file for encoding
+            return None
+        
         with open(file, "rb") as f:
             contents = f.read()
-        #only framing header and size of file
+        
         file_size = len(contents)
         file_header = f"{file}\n{file_size}\n".encode()
 
-        os.write(1, file_header)
-        os.write(1, contents)
-#sends file to server
-def sender(connection, file):
-    with open(file, 'rb') as f:
-        fr = f.read()
-    framed_file = outBandFrame(file)
-    connection.sendall(framed_file)
-    connection.sendall(b'eof')
+        framed_data += file_header + contents
     
-#acknowledgement from server verification
+    return framed_data
+
+def sender(connection, file):
+    framed_file = outBandFrame([file])
+    if framed_file is not None:
+        connection.sendall(framed_file)
+        connection.sendall(b'eof')
+
 def ack(connection):
     ack = connection.recv(1024).decode()
-    print(f"Acknowledgment recieved: {ack}")
-#executes processes and sends to correct server
+    print(f"Acknowledgment received: {ack}")
+
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(('127.0.0.1',50000))
+    sock.connect(('127.0.0.1', 50000))
 
     file = sys.argv[1]
-    print("Client wants to send: {file}")
-    sender(sock,file)
+    print(f"Client wants to send: {file}")
+    sender(sock, file)
     print("Client sent file successfully")
 
     ack(sock)
--- basicServer.py
+++ basicServer.py
@@ -1,26 +1,70 @@
-#! usr/bin/env python3
+#! /usr/bin/env python3
 
 import socket, os, sys, re, time
+from io import BufferedReader, FileIO
+
+def outBandDeframe(files):
+    extracted_files,extracted_contents = [], []
+    #error handling
+    if not os.path.exists(files):                              
+        os.write(2,("Framed file %s does not exist\n" % files).encode())
+        exit()
+    #opening framed file for extraction
+    fdReader = os.open(files, os.O_RDONLY)                     
+    read_file = BufferedReader(FileIO(fdReader))       
+    
+    while True:
+        #decode file
+        fname = read_file.readline().decode().rstrip()    
+
+        if not fname:                                  
+            break
+        #creates new file, 2.0 version of framed file
+        fname = f"{fname.split('.')[0]}2"
+        fdWriter = os.open(fname, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, stat.S_IRWXU)
+        #error handling
+        fsize_str  = read_file.readline().decode().rstrip()
+        if not  fsize_str:
+            break
+        fsize = int(fsize_str)
+        contents = read_file.read(fsize)
+        os.write(fdWriter,contents)
+        extracted_files.append(fname)
+        extracted_contents.append(contents)
+
+    tar_file.close()
+
+    return extracted_files, extracted_contents
+
 
 #processing client's file
-def receiver(connection, filename):
+def receiver(connection):
     folder = "transferred-files"
-    os.makedirs(folder_name, exist_ok=True)
-
+    os.makedirs(folder, exist_ok=True)
+    filename = connection.recv(1024).decode()
     file_path = os.path.join(folder, filename)
-    #makes sure that pipe is created for file
+
     if os.path.isfile(file_path):
         os.remove(file_path)
         os.mkfifo(file_path)
     else:
         os.mkfifo(file_path)
-    #processes file data
-    with open(file_path, 'wb') as file:
+
+    framed_data = b""
+
+    with open(file_path,'wb') as file:
         while True:
             data = connection.recv(1024)
-            if not data or data == b'end of file':
+            if not data:
                 break
             file.write(data)
+            framed_data += data
+    extracted_files, extracted_contents = outBandDeframe(file_path)
+
+    os.remove(file_path)
+
+    return extracted_files, extracted_contents
+    
 
 #sends acknowledgement message to client
 def ack(connection, ack_message):
@@ -42,11 +86,13 @@ def main():
     connection, address = server_socket.accept()
     print(f"Connection from {address}")
 
-    received_filename = connection.recv(1024).decode()
-    print(f"Receiving file: {received_filename}")
+    incoming = connection.recv(1024).decode()
 
-    receiver(connection, received_filename)
-    print("File has been successfully received.")
+    print(f"Receiving file: {incoming}")
+
+    received_filename,received_contents = receiver(connection)
+    if received_filename and recieved_contents != None:
+        print("File has been successfully received.")
 
     ack_message = "Acknowledged"
     ack(connection, ack_message)
