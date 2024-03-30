00eb6e9bd505b6f793bd454a44b815bf219578c0
cemacdonald <cemacdonald2@miners.utep.edu>
2024-03-29 20:58:21 -0600

    added fork version

---
+++ forkClient.py
@@ -0,0 +1,47 @@
+#! /usr/bin/env python3
+import sys
+import socket
+import os
+
+def outBandFrame(files):
+    framed_data = b""
+    for file in files:
+        if not os.path.exists(file):
+            print(f"File {file} does not exist", file=os.fdopen(2, 'w'))
+            return None
+        
+        with open(file, "rb") as f:
+            contents = f.read()
+        
+        file_size = len(contents)
+        file_header = f"{file}\n{file_size}\n".encode()
+
+        framed_data += file_header + contents
+    
+    return framed_data
+
+def sender(connection, file):
+    framed_file = outBandFrame([file])
+    if framed_file is not None:
+        connection.sendall(framed_file)
+        connection.sendall(b'eof')
+
+def ack(connection):
+    ack = connection.recv(1024).decode()
+    print(f"Acknowledgment received: {ack}")
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(('127.0.0.1', 50000))
+
+    file = sys.argv[1]
+    print(f"Client wants to send: {file}")
+    sender(sock, file)
+    print("Client sent file successfully")
+
+    ack(sock)
+    print("Client is tired of sending file, closing time!")
+    sock.close()
+
+if __name__ == "__main__":
+    main()
---
+++ forkServer.py
@@ -0,0 +1,115 @@
+#! /usr/bin/env python3
+
+import socket, os, sys, re, time
+from io import BufferedReader, FileIO
+
+def outBandDeframe(files):
+    extracted_files,extracted_contents = [], []
+    #error handling
+    if not os.path.exists(files):                              
+        os.write(2,("Framed file %s does not exist\n" % files).encode())
+        exit()
+    #opening framed file for extraction
+    fdReader = os.open(files, os.O_RDONLY)                     
+    read_file = BufferedReader(FileIO(fdReader))       
+    
+    while True:
+        #decode file
+        fname = read_file.readline().decode().rstrip()    
+
+        if not fname:                                  
+            break
+        #creates new file, 2.0 version of framed file
+        fname = f"{fname.split('.')[0]}2"
+        fdWriter = os.open(fname, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, stat.S_IRWXU)
+        #error handling
+        fsize_str  = read_file.readline().decode().rstrip()
+        if not  fsize_str:
+            break
+        fsize = int(fsize_str)
+        contents = read_file.read(fsize)
+        os.write(fdWriter,contents)
+        extracted_files.append(fname)
+        extracted_contents.append(contents)
+
+    tar_file.close()
+
+    return extracted_files, extracted_contents
+
+
+#processing client's file
+def receiver(connection):
+    folder = "transferred-files"
+    os.makedirs(folder, exist_ok=True)
+    filename = connection.recv(1024).decode()
+    file_path = os.path.join(folder, filename)
+
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+        os.mkfifo(file_path)
+    else:
+        os.mkfifo(file_path)
+
+    framed_data = b""
+
+    with open(file_path,'wb') as file:
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            file.write(data)
+            framed_data += data
+    extracted_files, extracted_contents = outBandDeframe(file_path)
+
+    os.remove(file_path)
+
+    return extracted_files, extracted_contents
+    
+
+#sends acknowledgement message to client
+def ack(connection, ack_message):
+    ack_message = ack_message.encode()
+    connection.sendall(ack_message)
+
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind(('127.0.0.1', 50000))
+    sock.listen(5)  # Maximum 5 pending connections
+
+    host = '127.0.0.1'
+    port = 50000
+    
+    print(f"Server is listening on {host}:{port}")
+
+    while True:
+        # Accept a new connection
+        connection, address = sock.accept()
+        print(f"Connection from {address}")
+
+        # Fork a new process
+        pid = os.fork()
+
+        if pid == 0:  # Child process
+            sock.close()  # Close server socket in child process
+            
+            incoming = connection.recv(1024).decode()
+            print(f"Receiving file: {incoming}")
+
+            received_filename, received_contents = receiver(connection)
+            if received_filename and received_contents:
+                print("File has been successfully received.")
+
+            ack_message = "Acknowledged"
+            ack(connection, ack_message)
+            print("Acknowledgement sent")
+
+            connection.shutdown(socket.SHUT_WR)
+            connection.close()
+            sys.exit(0)  # Exit child process
+
+        else:  # Parent process
+            connection.close()  # Close connection socket in parent process
+
+if __name__ == "__main__":
+    main()
